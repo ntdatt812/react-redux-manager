f6b3da69541375c3a51a365a8afc00441d24e42d
ntdatt812 <ntdat812@gmail.com>
2024-11-23 20:48:16 +0700

    design

--- src/App.tsx
+++ src/App.tsx
@@ -1,52 +1,13 @@
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import Header from './components/layout/header'
+import TabBar from './components/layout/tabbar'
+import UserTable from './components/user/table.user'
 
-import { decreasing, increment } from './redux/counter/counter.slide'
-import { useAppDispatch, useAppSelector } from './redux/hooks'
-import { Button } from 'react-bootstrap'
-
-function App() {
-
-  // const count = useSelector((state: RootState) => state.counter)
-  const count = useAppSelector(state => state.counter)
-  const dispatch = useAppDispatch();
-
-  console.log(">>check count: ", count)
+const App = () => {
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <div>
-        <h1>
-          My current count = {count.value}
-          <Button>Test bootstrap</Button>
-        </h1>
-        <div style={{ display: "flex", justifyContent: 'center' }}>
-          <div >
-            <button
-              onClick={() => dispatch(
-                increment()
-              )}
-            >Increase + 10</button>
-          </div>
-          <div>
-            <button
-              onClick={() => {
-                dispatch(
-                  decreasing()
-                )
-              }}
-            >Decreasing - 1</button>
-          </div>
-        </div>
-
-      </div >
+      <Header />
+      <TabBar />
+      <UserTable />
     </>
   )
 }
---
+++ src/components/layout/tabbar.tsx
@@ -0,0 +1,22 @@
+import Tab from 'react-bootstrap/Tab';
+import Tabs from 'react-bootstrap/Tabs';
+import UserTable from '../user/table.user';
+
+const TabBar = () => {
+    return (
+        <Tabs
+            defaultActiveKey="profile"
+            id="uncontrolled-tab-example"
+            className="mb-3"
+        >
+            <Tab eventKey="user" title="Users">
+                <UserTable />
+            </Tab>
+            <Tab eventKey="Blog" title="Blogs">
+                Tab content for blogs
+            </Tab>
+        </Tabs>
+    );
+}
+
+export default TabBar;
